5ba62470d74633014cec9610a280174ec245927c
satocchi0416sh <83898438+satocchi0416sh@users.noreply.github.com>
2024-07-22 17:19:09 +0900

    feat: :rocket: スピナーを追加

--- feature/populations/components/PopulationChart.tsx
+++ feature/populations/components/PopulationChart.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useRef, useEffect, useState } from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { GridLoader } from 'react-spinners';
 import { usePrefectureContext } from '@/feature/prefectures';
 import { usePopulationCategoryContext, usePopulationData } from '..';
 
@@ -27,7 +28,6 @@ const PopulationChart = () => {
         };
     }, []);
 
-    if (loading) return <p>Loading...</p>;
     if (error) return <p>Error loading population data.</p>;
 
     const mergedData: { year: number;[key: string]: number }[] = [];
@@ -54,7 +54,7 @@ const PopulationChart = () => {
         let color = '#';
         for (let i = 0; i < 3; i++) {
             const value = (hash >> (i * 8)) & 0xFF;
-            color += ('00' + value.toString(16)).substr(-2);
+            color += value.toString(16).padStart(2, '0');
         }
         return color.replace(/^(#)(.{2})(.{2})(.{2})$/, (_, hash, r, g, b) => {
             const adjust = (c: string) => Math.max(0, Math.min(255, parseInt(c, 16) - 50)).toString(16).padStart(2, '0');
@@ -65,7 +65,11 @@ const PopulationChart = () => {
     return (
         <div className='bg-white mt-5 p-10 rounded-lg'>
             <h2 className='text-lg font-bold mb-5' ref={chartContainerRef}>{selectedPopulationCategory}</h2>
-            {populationData.length > 0 ? (
+            {loading ? (
+                <div className='flex justify-center items-center h-[500px]'>
+                    <GridLoader color="#36d7b7" />
+                </div>
+            ) : populationData.length > 0 ? (
                 <LineChart width={chartWidth} height={500} data={mergedData}>
                     <XAxis dataKey="year" />
                     <YAxis />
@@ -83,7 +87,9 @@ const PopulationChart = () => {
                     ))}
                 </LineChart>
             ) : (
-                <p>No prefectures selected</p>
+                <div className='flex justify-center items-center h-[500px]'>
+                    <p>No prefectures selected</p>
+                </div>
             )}
         </div>
     );
