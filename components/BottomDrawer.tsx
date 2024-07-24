e88e8ecdfe070b16c08fc17600c0f47a10a9d824
satocchi0416sh <83898438+satocchi0416sh@users.noreply.github.com>
2024-07-24 23:59:16 +0900

    fix: :rotating_light: Imageを最も上でインポート

--- components/BottomDrawer.tsx
+++ components/BottomDrawer.tsx
@@ -1,8 +1,8 @@
 "use client"
+import Image from 'next/image'
 import React from 'react'
 import { PrefectureList } from '@/feature/prefectures'
 import { ChevronLeft } from '@/public'
-import Image from 'next/image'
 
 interface BottomDrawerProps {
     isOpen: boolean
