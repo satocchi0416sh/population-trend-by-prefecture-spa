362b038fd622adbabad98c5475b1aec5c756cf3f
satocchi0416sh <83898438+satocchi0416sh@users.noreply.github.com>
2024-07-24 23:56:38 +0900

    feat: :rocket: Sidebar内でスクロールするように

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,9 +3,11 @@ import { PrefectureList } from '@/feature/prefectures'
 
 const Sidebar = () => {
     return (
-        <div className='w-1/4 min-w-[310px] border-r-2 border-gray-200 p-10 overflow-y-auto hidden md:block bg-white'>
-            <h2 className='text-xl font-bold mb-10'>都道府県</h2>
-            <PrefectureList />
+        <div className='w-1/4 min-w-[330px] border-r-2 border-gray-200 hidden md:block bg-white'>
+            <div className='h-[calc(100vh-70px)] p-10 overflow-y-scroll '>
+                <h2 className='text-xl font-bold mb-10'>都道府県</h2>
+                <PrefectureList />
+            </div>
         </div>
     )
 }
