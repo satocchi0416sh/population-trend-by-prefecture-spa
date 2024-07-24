cd5925a1cd95ecb62255405a197c9304807a2c35
satocchi0416sh <83898438+satocchi0416sh@users.noreply.github.com>
2024-07-24 23:49:15 +0900

    fix: :bug: PC版ではBottomDrawerが表示されないように

--- components/BottomDrawerButton.tsx
+++ components/BottomDrawerButton.tsx
@@ -7,7 +7,7 @@ interface BottomDrawerButtonProps {
 
 const BottomDrawerButton: React.FC<BottomDrawerButtonProps> = ({ isOpen, handleChange }) => {
     return (
-        <button className={`fixed bottom-5 left-0 right-0 bg-accent-01 text-white font-bold mx-4 rounded-full py-4 justify-center items-center z-30 ${isOpen ? 'hidden' : 'block'}`}
+        <button className={`fixed bottom-5 left-0 right-0 bg-accent-01 text-white font-bold mx-4 rounded-full py-4 justify-center items-center z-30 ${isOpen ? 'hidden' : 'block'} md:hidden`}
             onClick={handleChange}
         >
             都道府県
